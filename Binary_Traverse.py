a1e479adbaa0decc8caec2f3e9b65f62ed6b1059
Sako <ssaakkooo@gmail.com>
2018-12-06 13:54:01 +0400

    Save

--- Binary_Traverse.py
+++ Binary_Traverse.py
@@ -1,42 +1,98 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
+        self.data = data
         self.left = None
         self.right = None
-        self.data = data
 
 
+class binary_tree:
+    def __init__(self):
+        self.root = None
 
-def Inorder(root):
-    if root:
-        Inorder(root.left)
-        print(root.data)
-        Inorder(root.right)
+    def insert(self, data):
+        if self.root == None:
+            self.root = Node(data)
+        else:
+            self._insert(data, self.root)
 
-def Postorder(root):
-    if root:
-        Postorder(root.left)
-        Postorder(root.right)
-        print(root.data)
-def Preorder(root):
-    if root:
-        print(root.data)
-        Preorder(root.left)
-        Preorder(root.right)
+    def _insert(self, data, cur_node):
+        if data < cur_node.data:
+            if cur_node.left == None:
+                cur_node.left = Node(data)
+            else:
+                self._insert(data, cur_node.left)
+        elif data > cur_node.data:
+            if cur_node.right == None:
+                cur_node.right = Node(data)
+
+            else:
+                self._insert(data, cur_node.right)
+        else:
+            print "Value already in tree!"
+
+    def Inorder(self):
+        print"\n"
+        print "Inorder Binary Tree Traverse is: "
+        if self.root != None:
+            self._Inorder(self.root)
+
+    def _Inorder(self, root):
+        if root != None:
+            self._Inorder(root.left)
+            print str(root.data),
+            self._Inorder(root.right)
+
+    def Preorder(self):
+        print"\n"
+        print "Preorder Binary Tree Traverse is: "
+        if self.root != None:
+            self._Preorder(self.root)
+
+    def _Preorder(self, root):
+        if root != None:
+            print str(root.data),
+            self._Preorder(root.left)
+            self._Preorder(root.right)
+
+    def Postorder(self):
+        print"\n"
+        print "Postorder Binary Tree Traverse is: "
+        if self.root != None:
+            self._Postorder(self.root)
+
+    def _Postorder(self, root):
+        if root != None:
+            self._Postorder(root.left)
+            self._Postorder(root.right)
+            print str(root.data),
 
 
 def main():
-    root = Node(1)
-    root.left = Node(2)
-    root.right = Node(3)
-    root.left.left = Node(4)
-    root.left.right = Node(5)
+    tree = binary_tree()
+    tree.root = Node(1)
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
 
-    print "Preorder Binary Tree Traverse is: "
-    Preorder(root)
-
-    print "Inorder Binary Tree Traverse is: "
-    Inorder(root)
-
-    print "Postorder Binary Tree Traverse is: "
-    Postorder(root)
+    tree.Inorder()
+    tree.Postorder()
+    tree.Preorder()
 main()
+
+# insert function in main2 is from video you provided
+def main2():
+    tree = binary_tree()
+
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+    tree.insert(5)
+
+    tree.Inorder()
+    tree.Postorder()
+    tree.Preorder()
+
+
+
